bdda239b5f0411342c43647bd020b7c6b8140fcf
Hani <elisasoto@liceovvh.cl>
2025-07-14 09:10:25 -0400

    ok

--- 06_condicionales IF-else/static/js/script.js
+++ 06_condicionales IF-else/static/js/script.js
@@ -4,8 +4,8 @@ console.log("Conexión con js establecida...");
 /* Edad para votar
 Solicita la edad del usuario mediante prompt().
 Muestra con alert() si puede votar
-(18 años o más) o no.
-*/
+(18 años o más) o no.*/
+
 function edadVotar() {
   let edad = parseInt(prompt("Ingrese su Edad: "));
   // Input conversión string a número
@@ -24,8 +24,8 @@ function edadVotar() {
   Pide una contraseña con prompt().
   Si es igual a "1234", muestra un mensaje
   de acceso permitido. En caso contrario,
-  muestra acceso denegado.
-*/
+  muestra acceso denegado.*/
+
 function validarPassword() {
   let clave = "1234";
   let pass = prompt("Ingrese su contraseña: ");
@@ -41,6 +41,7 @@ function validarPassword() {
 Verificar par o impar
 Pide un número por prompt(). Evalúa si el número
 es par o impar y muestra el resultado con alert().*/
+
 // ejemplo de MOD --> modulo de división = resto
 function parImpar() {
   let numero = parseInt(prompt("Ingresar Numero"));
@@ -51,12 +52,11 @@ function parImpar() {
     alert("Es impar");
   }
 }
-/*
-Temperatura ambiental
+/*Temperatura ambiental
 Solicita la temperatura actual. Si es mayor o
 igual a 30, muestra “Hace calor”, de lo contrario
-muestra “Clima agradable”.
-*/
+muestra “Clima agradable”.*/
+
 function temperaturaAmbiental() {
   let temperatura = parseInt(prompt("Ingresar Temperatura"));
   if (temperatura >= 30 && temperatura <= 50) {
@@ -72,10 +72,7 @@ function temperaturaAmbiental() {
 
 /*Comparar dos números
  Solicita dos números al usuario.
-  Compara ambos y muestra cuál es mayor o si son iguales.
-
-
- */
+ Compara ambos y muestra cuál es mayor o si son iguales.*/
 
 function compararNumeros() {
   let num1 = parseInt(prompt("ingresar primer numero"));
@@ -83,31 +80,33 @@ function compararNumeros() {
   if (num1 < num2) {
     alert("el numero " + num1 + " es menor que " + num2);
   }
-  else if(num1 == num2){
+  else if (num1 == num2) {
     alert("el numero " + num1 + " es igual que " + num2);
   }
-  else{
+  else {
     alert("el numero " + num1 + " es mayor que " + num2);
   }
 }
 /* Calificación escolar: Pide una nota entre 1 y 7. Si
 es 4 o más, muestra “Aprobado”, si es menor, muestra “Reprobado”. */
 
-function note(){
+function note() {
   let ingresa = parseInt(prompt("ingresa tu nota:"));
 
-  if (ingresa < 4){
+  if (ingresa < 4) {
     alert("desaprobado");
-  } else if (ingresa >= 4 && ingresa <=7){
+  } else if (ingresa >= 4 && ingresa <= 7) {
     alert("aprobado");
   } else {
-    alert ("por favor, ingresar valor valido");
+    alert("por favor, ingresar valor valido");
   }
 };
 
-// Pide un nombre de usuario. Si el valor ingresado es "admin", muestra “Bienvenido, administrador”. Si no, muestra “Usuario no reconocido”.
+/* Pide un nombre de usuario. 
+Si el valor ingresado es "admin", muestra “Bienvenido, administrador”. 
+ Si no, muestra “Usuario no reconocido”.*/
 
-function usuarioAdmin(){
+function usuarioAdmin() {
   let usu = prompt("Ingrese usuario")
   if (usu == "admin") {
     alert("Bienvenido, Administrador")
@@ -116,11 +115,12 @@ function usuarioAdmin(){
   }
 }
 
-//  Pide una palabra al usuario. Verifica si empieza con la letra “A” mayúscula y muestra un mensaje acorde.
+/* Pide una palabra al usuario. 
+Verifica si empieza con la letra “A” mayúscula y muestra un mensaje acorde.*/
 
-function palabraMayuscula(){
+function palabraMayuscula() {
   let letra = prompt("Ingresa palabra");
-  let letraA = "A" ;
+  let letraA = "A";
   if (letra[0] == letraA) {
     alert("la palabra comienza con a mayuscula");
   } else {
@@ -128,18 +128,43 @@ function palabraMayuscula(){
   }
 }
 
-function precio(){ // Creación de la función
+/* Solicita el precio de un producto. 
+Si el precio es mayor a 10000, muestra que aplica descuento.
+ Si no, indica precio normal.*/
+
+function precio() { // Creación de la función
   //Añadir un porcentaje de descuento e IVA y mostrar.
   let iva = 0.19;
   let descuento = parseFloat(prompt("Ingrese el valor de descuento: Ejemplo 20"));
   descuento /= 100; // operación para llevar a decimal
   let precio = parseFloat(prompt("Ingrese el precio del producto"));
-  if (precio > 10000){
+  if (precio > 10000) {
     alert(`El valor ingresado fue: ${precio}\n
           IVA: ${precio * iva}\n
           Precio Total:${precio * 1.19}\n
           `);
   } else {
     alert("Precio normal");
+  }
+}
+
+/*Solicita la edad del usuario y si tiene licencia (por ejemplo, respondiendo "sí" o "no"). 
+Si tiene 18 o más y respondió que tiene licencia, muestra “Puede conducir”.
+Si no, muestra “No puede conducir”.*/
+
+function licenciaEdad() {
+  let edad = parseInt(prompt("Ingrese su edad: "));
+
+  if (edad >= 18 && edad < 100) {
+    let licencia = prompt("Indica si tienes licencia si/no");
+    if (licencia == "si") {
+      alert("Usted puede conducir!");
+    } else if (licencia == no) {
+      alert("No puede conducir!")
+    } else {
+      alert("Ingrese valores validos!")
+    }
+  } else {
+    alert("No puede conducir!")
   }
 }
